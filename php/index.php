f894d15c90c3a261b0c92cbe20eceef02b1461b5
gabrielleal23 <110639928+gabrielleal23@users.noreply.github.com>
2025-05-29 21:13:06 -0500

    primercargue

---
+++ php/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba de cargue en la nube</title>
+</head>
+<body>
+    <h1>Prueba de cargue en la nube</h1>
+    
+</body>
+</html>
