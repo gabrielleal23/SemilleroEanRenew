2bd7e5d10a050fd3950e37cdfb3063b51f559d28
gabrielleal23 <110639928+gabrielleal23@users.noreply.github.com>
2025-06-04 22:39:38 -0500

    pruebas2

--- php/register.php
+++ php/register.php
@@ -7,6 +7,27 @@
     <link rel="icon" href="../icon/ean_logo.png" type="image/png">
     <link rel="stylesheet" href="../css/styles.css">
     
+    <style>
+    .seleccionador {
+      width: 400px;
+      height: 35px;
+      border-radius: 10px;
+      border: 1px solid #ccc;
+      padding: 5px 10px;
+      font-size: 16px;
+      background-color: white;
+      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+      appearance: none;
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      background-image: url("data:image/svg+xml;utf8,<svg fill='%23666' height='24' viewBox='0 0 24 24' width='24' xmlns='http://www.w3.org/2000/svg'><path d='M7 10l5 5 5-5z'/></svg>");
+      background-repeat: no-repeat;
+      background-position: right 10px center;
+      background-size: 20px;
+      padding-right: 35px;
+    }
+    </style>
+    
 
 </head>
 <body>
@@ -18,7 +39,7 @@
         <input type="text" placeholder="Correo" required>
         <input type="text" placeholder="Telefono" required>
         <input type="text" placeholder="Contraseña" required>
-          <select class = 'seleccionador' name="role" id="role" required>
+          <select class = "seleccionador" name="role" id="role" required>
             <option value="estudiante">Estudiante</option>
             <option value="tutor">Tutor</option>
         </select>
