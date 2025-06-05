f244f09dc0576c867d85e39a0b83d8c7653b5200
gabrielleal23 <110639928+gabrielleal23@users.noreply.github.com>
2025-06-04 23:13:31 -0500

    ajuste segundo interview

--- php/login.php
+++ php/login.php
@@ -5,13 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrate</title>
     <link rel="icon" href="../icon/ean_logo.png" type="image/png">
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/style2.css">
 
 </head>
 <body>
     <form action="">
     <div class = "form-container">
-        <h2 style="text-align: center;">Iniciar Sesion</h2>
+        <center>
+            <img src="../icon/ean_logo.png" alt="ean" style="width: 50px;padding-bottom: 0px;">
+            <h2 style="text-align: center;">Iniciar Sesion</h2>
+        </center>
         <input type="text" placeholder="Correo" required>
         <input type="text" placeholder="Contraseña" required>
         <button type="submit">Iniciar Sesion</button>
--- php/register.php
+++ php/register.php
@@ -5,35 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrate</title>
     <link rel="icon" href="../icon/ean_logo.png" type="image/png">
-    <link rel="stylesheet" href="../css/styles.css">
-    
-    <style>
-    .seleccionador {
-      width: 400px;
-      height: 35px;
-      border-radius: 10px;
-      border: 1px solid #ccc;
-      padding: 5px 10px;
-      font-size: 16px;
-      background-color: white;
-      box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-      appearance: none;
-      -webkit-appearance: none;
-      -moz-appearance: none;
-      background-image: url("data:image/svg+xml;utf8,<svg fill='%23666' height='24' viewBox='0 0 24 24' width='24' xmlns='http://www.w3.org/2000/svg'><path d='M7 10l5 5 5-5z'/></svg>");
-      background-repeat: no-repeat;
-      background-position: right 10px center;
-      background-size: 20px;
-      padding-right: 35px;
-    }
-    </style>
-    
+    <link rel="stylesheet" href="../css/style2.css">
+
 
 </head>
 <body>
     <form action="" method="post">
     <div class = "form-container">
-        <h2 style="text-align: center;">Registro Para El Semillero</h2>
+        <center>
+            <img src="../icon/ean_logo.png" alt="ean" style="width: 50px;padding-bottom: 0px;">
+            <h2 style="text-align: center;">Registro Para El Semillero</h2>
+        </center>
         <input type="text" placeholder="Nombre" required>
         <input type="text" placeholder="Cedula" required>
         <input type="text" placeholder="Correo" required>
